45d49ae20624e089375e2ddf6658499115f333d3
mohammad joma <150864877+mohammadbjoma@users.noreply.github.com>
2026-01-03 21:00:13 +0200

    js solution

--- 5. Longest Palindrom substring/solution.js
+++ 5. Longest Palindrom substring/solution.js
@@ -1,3 +1,26 @@
 var longestPalindrome = function(s) {
-    
+    if (s.length < 2) return s;
+
+    let start = 0, end = 0;
+
+    function expand(left, right) {
+        while (left >= 0 && right < s.length && s[left] === s[right]) {
+            left--;
+            right++;
+        }
+        return right - left - 1; // palindrome length
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        let len1 = expand(i, i);     // odd length
+        let len2 = expand(i, i + 1); // even length
+        let len = Math.max(len1, len2);
+
+        if (len > end - start) {
+            start = i - Math.floor((len - 1) / 2);
+            end = i + Math.floor(len / 2);
+        }
+    }
+
+    return s.substring(start, end + 1);
 };
