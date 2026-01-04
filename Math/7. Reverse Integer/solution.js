cfefb47678856e69193e9d5f3628eba198352121
mohammad joma <150864877+mohammadbjoma@users.noreply.github.com>
2026-01-05 00:04:07 +0200

    js solution

---
+++ Math/7. Reverse Integer/solution.js
@@ -0,0 +1,14 @@
+var reverse = function(x) {
+    const isNegative = x < 0;
+    let num = Math.abs(x);
+    let reversed = 0;
+
+    while (num > 0) {
+        let digit = num % 10;
+        reversed = reversed * 10 + digit;
+        num = Math.floor(num / 10);
+    }
+    if (reversed > 2 ** 31 - 1) return 0;
+
+    return isNegative ? -reversed : reversed;
+};
