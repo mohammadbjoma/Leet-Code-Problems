f7a3e3a4363fa47d5cc1631355f78de8cfdfea8f
mohammad joma <150864877+mohammadbjoma@users.noreply.github.com>
2026-01-08 11:05:22 +0200

    Climbing Stairs solution

---
+++ Math/70. Climbing Stairs/solution.js
@@ -0,0 +1,14 @@
+var climbStairs = function(n) {
+    if(n===1)return 1;
+    if(n===2) return 2;
+    let first=1;
+    let second=2;
+        for (let i = 3; i <= n; i++) {
+        let current = first + second; 
+        first = second;
+        second = current;
+    }
+    
+    return second;
+
+};
