825d592326527a2b3194c3afc2c43dde5a520090
mohammad joma <150864877+mohammadbjoma@users.noreply.github.com>
2026-01-16 19:09:39 +0200

    Container With Most Water solution

---
+++ Arrays/11. Container With Most Water/solution.js
@@ -0,0 +1,19 @@
+var maxArea = function(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let max = 0;
+
+    while (left < right) {
+        let h = Math.min(height[left], height[right]);
+        let w = right - left;
+        max = Math.max(max, h * w);
+
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return max;
+};
