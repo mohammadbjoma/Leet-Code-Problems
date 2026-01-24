baca8057f922c47a78ef274fa7c49390a96dfc55
mohammad joma <150864877+mohammadbjoma@users.noreply.github.com>
2026-01-24 23:19:14 +0200

    Find All Numbers Disappeared in an Array solution

---
+++ Arrays/448. Find All Numbers Disappeared in an Array/solution.js
@@ -0,0 +1,13 @@
+var findDisappearedNumbers = function(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        const idx = Math.abs(nums[i]) - 1;
+        if (nums[idx] > 0) nums[idx] = -nums[idx];
+    }
+
+    const res = [];
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > 0) res.push(i + 1);
+    }
+
+    return res;
+};
