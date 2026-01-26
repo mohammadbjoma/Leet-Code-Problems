58fa10e92436a274220f2e9e71a962f61a0eabaa
mohammad joma <150864877+mohammadbjoma@users.noreply.github.com>
2026-01-26 23:39:02 +0200

    Swap Nodes in Pairs solution

---
+++ Linked List/24. Swap Nodes in Pairs/solution.js
@@ -0,0 +1,20 @@
+var swapPairs = function(head) {
+    let dummy = new ListNode(0);
+    dummy.next = head;
+
+    let prev = dummy;
+
+    while (prev.next && prev.next.next) {
+        let first = prev.next;
+        let second = first.next;
+
+  
+        first.next = second.next;
+        second.next = first;
+        prev.next = second;
+
+        prev = first;
+    }
+
+    return dummy.next;
+};
